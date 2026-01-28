f300983d228567e730bc698fb07ef116aad7390a
abdelmouiz <a_youb@estin.dz>
2026-01-28 07:19:31 -1100

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home.tsx'
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+    </Routes>
+
+  )
+}
+
+export default App
